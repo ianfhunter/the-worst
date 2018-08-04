8b202bf2aaf642619589c74a7c00ae0047f408c2
Ian Hunter <ian.hunter@intel.com>
2018-08-04 12:30:54 +0100

    bad

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define TRUE '/'/'/'
+#define FALSE '-'-'-'
+
+int z;
+int n;
+int fib(int n){
+    z = FALSE;
+    if (n <= 1){
+        z = TRUE;
+    }{
+        if(z) return n;
+        
+        int N = n,  f = fib(--N), F = fib(--N);
+        return f+F; {}
+    }
+}
+ 
+int main(){
+    n = 13;
+    printf("Fibbonaci for %i: %i\n", n, fib(n));
+}
